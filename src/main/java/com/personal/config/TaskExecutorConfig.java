b1d63da7b9a7503afb4ae9a0176bf1a32c3c8b74
Joseph Barbati <jrbarbati@icloud.com>
2025-05-01 01:10:03 -0400

    initial commit

---
+++ src/main/java/com/personal/config/TaskExecutorConfig.java
@@ -0,0 +1,25 @@
+package com.personal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableScheduling
+public class TaskExecutorConfig
+{
+    @Bean
+    public TaskExecutor taskExecutor()
+    {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("main-executor-");
+
+        return executor;
+    }
+}
